a7f5c2438c79048c1d3fc334b7e72547197c9db3
hafan-961 <muhammedhafan961@gmail.com>
2026-07-25 23:44:05 +0530

    pointers

---
+++ pointer/derefference.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int* ptr = &a;
+    int** parptr = &ptr;
+
+    cout << *ptr << endl;
+    cout << **parptr << endl;
+    cout << ptr << endl;
+    cout << *parptr << endl;
+    cout << &a << endl;
+    
+}

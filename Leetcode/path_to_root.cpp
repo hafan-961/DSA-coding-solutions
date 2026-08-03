1b4c730534900d93c1d906518dbaf8c84e818e9a
hafan-961 <muhammedhafan961@gmail.com>
2026-08-03 22:05:21 +0530

    path to leaf node of a tree

---
+++ Leetcode/path_to_root.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void traversal(TreeNode* root, string path , vector<string> &ans){
+        if(root == nullptr){
+            return;
+        }
+
+        if(path.empty()){
+            path = to_string(root->val);
+        } else {
+            path = path + "->" + to_string(root->val);
+        }
+
+        if(root->left == nullptr && root->right == nullptr){
+            ans.push_back(path);
+            return;
+
+        }
+
+        traversal(root->left,path, ans);
+        traversal(root->right,path,ans);
+
+    }
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> ans;
+        traversal(root ,"", ans);
+        return ans;
+    }
+};
